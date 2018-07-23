0e346fecdf8375e088f0bfed7296076acddc161d
snazzafrass <snazzafrass@gmail.com>
2018-07-23 19:30:57 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,8 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+
+ice cream= "chocolate vanilla swirl with cookie crunch please"
+
+
+
+puts "YAS! I did it! I got #{ice cream}!"
